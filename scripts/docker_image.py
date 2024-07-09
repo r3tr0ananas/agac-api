ee4c9b5d687a39b92be4c391e4a8ddb4fe3f92be
Ananas <ananas@ananas.moe>
2024-07-09 04:22:54 +0200

    feat: docker build script

---
+++ scripts/docker_image.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, ".")
+
+import os
+from api import __version__
+
+build_name = "r3tr0ananas/agac_api"
+
+os.system(
+    f"docker build -t {build_name}:{__version__} --build-arg ARCH=amd64 ."
+)
+
+os.system(
+    f"docker build -t {build_name}:latest --build-arg ARCH=amd64 ."
+)
